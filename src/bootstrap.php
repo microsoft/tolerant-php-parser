2be73113a8526ea7c5e48ecfdd9bf16332f003fb
Sara Itani <sitani@microsoft.com>
2017-01-13 16:48:34 -0800

    update autoload

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -5,7 +5,5 @@
  *--------------------------------------------------------------------------------------------*/
 
 spl_autoload_register(function ($class) {
-    if (\file_exists($filepath = __DIR__ . "/" . \substr($class, 10) . ".php")) {
-        require_once $filepath;
-    }
+    require_once str_replace("\\", "/", __DIR__ . "/" . \substr($class, 10) . ".php");
 });
