5053c5cdd618ce1f45d649971f8231eb6048ab62
Sara Itani <sitani@microsoft.com>
2017-04-10 14:24:04 -0700

    enable single node checks diagnostics api

--- src/DiagnosticsProvider.php
+++ src/DiagnosticsProvider.php
@@ -17,9 +17,7 @@ class DiagnosticsProvider {
 
     private static $tokenKindToText;
 
-    public static function getDiagnostics(Node $n) : array {
-        $diagnostics = [];
-
+    public static function checkDiagnostics($node) {
         if (!isset(self::$tokenKindToText)) {
             self::$tokenKindToText = \array_flip(\array_merge(
                 TokenStringMaps::OPERATORS_AND_PUNCTUATORS,
@@ -28,51 +26,60 @@ class DiagnosticsProvider {
             ));
         }
 
-        foreach ($n->getDescendantNodesAndTokens() as $node) {
-            if ($node instanceof SkippedToken) {
-                // TODO - consider also attaching parse context information to skipped tokens
-                // this would allow us to provide more helpful error messages that inform users what to do
-                // about the problem rather than simply pointing out the mistake.
-                $diagnostics[] = new Diagnostic(
-                    DiagnosticKind::Error,
-                    "Unexpected '" .
-                    (isset(self::$tokenKindToText[$node->kind])
-                        ? self::$tokenKindToText[$node->kind]
-                        : Token::getTokenKindNameFromValue($node->kind)) .
-                    "'",
-                    $node->start,
-                    $node->getEndPosition() - $node->start
-                );
-            } elseif ($node instanceof MissingToken) {
-                $diagnostics[] = new Diagnostic(
-                    DiagnosticKind::Error,
-                    "'" .
-                    (isset(self::$tokenKindToText[$node->kind])
-                        ? self::$tokenKindToText[$node->kind]
-                        : Token::getTokenKindNameFromValue($node->kind)) .
-                    "' expected.",
-                    $node->start,
-                    $node->getEndPosition() - $node->start
-                );
-            }
+        if ($node instanceof SkippedToken) {
+            // TODO - consider also attaching parse context information to skipped tokens
+            // this would allow us to provide more helpful error messages that inform users what to do
+            // about the problem rather than simply pointing out the mistake.
+            return new Diagnostic(
+                DiagnosticKind::Error,
+                "Unexpected '" .
+                (isset(self::$tokenKindToText[$node->kind])
+                    ? self::$tokenKindToText[$node->kind]
+                    : Token::getTokenKindNameFromValue($node->kind)) .
+                "'",
+                $node->start,
+                $node->getEndPosition() - $node->start
+            );
+        } elseif ($node instanceof MissingToken) {
+            return new Diagnostic(
+                DiagnosticKind::Error,
+                "'" .
+                (isset(self::$tokenKindToText[$node->kind])
+                    ? self::$tokenKindToText[$node->kind]
+                    : Token::getTokenKindNameFromValue($node->kind)) .
+                "' expected.",
+                $node->start,
+                $node->getEndPosition() - $node->start
+            );
+        }
 
-            if ($node === null || $node instanceof Token) {
-                continue;
-            }
+        if ($node === null || $node instanceof Token) {
+            return null;
+        }
 
-            if ($node instanceof Node) {
-                if ($node instanceof Node\MethodDeclaration) {
-                    foreach ($node->modifiers as $modifier) {
-                        if ($modifier->kind === TokenKind::VarKeyword) {
-                            $diagnostics[] = Diagnostic(
-                                DiagnosticKind::Error,
-                                "Unexpected modifier '" . self::$tokenKindToText[$modifier->kind] . "'",
-                                $modifier->start,
-                                $modifier->length
-                            );
-                        }
+        if ($node instanceof Node) {
+            if ($node instanceof Node\MethodDeclaration) {
+                foreach ($node->modifiers as $modifier) {
+                    if ($modifier->kind === TokenKind::VarKeyword) {
+                        return Diagnostic(
+                            DiagnosticKind::Error,
+                            "Unexpected modifier '" . self::$tokenKindToText[$modifier->kind] . "'",
+                            $modifier->start,
+                            $modifier->length
+                        );
                     }
                 }
+            }
+        }
+        return null;
+    }
+
+    public static function getDiagnostics(Node $n) : array {
+        $diagnostics = [];
+        
+        foreach ($n->getDescendantNodesAndTokens() as $node) {
+            if (($diagnostic = self::checkDiagnostics($node)) !== null) {
+                $diagnostics[] = self::checkDiagnostics($node);
             }
         }
 
