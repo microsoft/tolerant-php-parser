59351ce2a6b32c6cafc667dc20a48da36f38a482
Rob Lourens <roblourens@gmail.com>
2018-02-14 10:39:41 -0800

    Add license to FilePositionMapTest.php

--- tests/FilePositionMapTest.php
+++ tests/FilePositionMapTest.php
@@ -1,4 +1,8 @@
 <?php
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 
 use Microsoft\PhpParser\FilePositionMap;
 use Microsoft\PhpParser\LineCharacterPosition;
